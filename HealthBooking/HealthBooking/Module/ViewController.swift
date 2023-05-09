2aa94932798f693d2cbf0426e9ed4a4520cf1b6d
ZHIYUAN FAN <130140234+fanzhaire@users.noreply.github.com>
2023-05-09 13:37:11 +1000

    healthbookingf
    personalinformation

--- HealthBooking/HealthBooking/Module/ViewController.swift
+++ HealthBooking/HealthBooking/Module/ViewController.swift
@@ -7,12 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class profile: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+   
 
 
 }
