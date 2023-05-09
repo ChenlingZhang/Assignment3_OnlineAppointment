b02b9ba052dad101f505838ee8c7e152d1b5a70d
Zhiyi He <zhiyihe@ZhiyideMacBook-Air.local>
2023-05-09 11:20:57 +1000

    init

---
+++ HealthBooking/HealthBooking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthBooking
+//
+//  Created by Zhiyi He on 3/5/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
